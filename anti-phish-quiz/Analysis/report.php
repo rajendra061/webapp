bd223e4e803887a889d36386d60944cf18c0c235
Rajendra B. Thapa <rajendrathapa@gmail.com>
2014-01-25 22:39:16 +0545

    Uploading the Web App
    This quiz can be played being anonymous or being registered.
    (http://upvedatech.com/quiz/)
    List of real websites and phishing websites are shown to the users as
    question. Users have to recognize whether it is real site or phishing
    website.

---
+++ anti-phish-quiz/Analysis/report.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Report</title>
+<link rel="stylesheet" type="text/css" href="quiz.css">
+<link rel="stylesheet" type="text/css" href="bootstrap.css">
+</head>
+
+<body>
+	<div class="body_wrapper">
+    	<div class="header_wrapper">
+        	<div class="report_title">
+            	Antiphising Report
+            </div>
+        </div>
+        <div class="menu_wrapper">
+        	<ul class="menu">
+            	<li><a href="#">Table</a></li>
+                <li><a href="#">Pie Chart</a></li>
+                <li><a href="#">Bar Graph</a></li>
+                <li><a href="#">sjafdja</a></li>
+                <li><a href="#">sjafdja</a></li>
+            </ul>
+        </div>
+        <div class="main_content_wrapper">
+        	<form name="report" class="report" id="report">
+            	<div class="form_content">
+                	<div class="form_label">
+                    	Question Number(1-20)
+                    </div>
+                    <div class="form_field">
+                    	<input type="text" name="name" id="name" class="form-control" />
+                    </div>
+                </div>
+                <div class="form_content">
+                	<div class="form_label">
+                    	Select Type
+                    </div>
+                    <div class="form_field">
+                    	<input type="radio" name="selecttype" id="selecttype" value="0" /><span>Table</span>
+                        <input type="radio" name="selecttype" id="selecttype" value="1" /><span>Bar Graph</span>
+                        <input type="radio" name="selecttype" id="selecttype" value="1" /><span>Pie Chart</span>
+                    </div>
+                </div>
+                <div class="form_content">
+                	<div class="form_label">
+                    	<input type="submit" value="Obtain Report" class="btn btn-primary" />
+                    </div>
+                    <div class="form_field">
+                    </div>
+                </div>
+                
+                 
+            </form>
+        </div>
+    </div>
+</body>
+</html>
